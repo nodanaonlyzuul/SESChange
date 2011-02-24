96f09aad7644ebe51bd1d9f64100a205d45a87fe
nodanaonlyzuul <stephen@eastmedia.com>
2011-02-24 01:56:23 -0500

    first commit

---
+++ ses_change_initializer.rb
@@ -0,0 +1,10 @@
+module SesChangeInitializer
+  def self.settings
+    settings ||= YAML.load_file(File.join('settings', 's3_credentials.yml'))
+  end
+
+  def self.ses_account
+    ses_account ||= AWS::SES::Base.new(:access_key_id     => settings["access_key_id"],
+                                       :secret_access_key => settings["secret_access_key_id"])
+  end
+end
---
+++ seschange.rb
@@ -0,0 +1,26 @@
+# TODO:
+#   ensure settings are only loading once
+#   README
+#   google charts
+#   gitignore
+#   embeded as rack app
+#   http_simple auth
+
+require 'rubygems'
+require 'sinatra'
+require 'aws/ses'
+require 'erb'
+require 'ses_change_initializer'
+
+before do
+  @app_name = "Stephen's Account"
+  @ses ||= SesChangeInitializer.ses_account
+end
+
+get '/' do
+  erb :index
+end
+
+get '/delivery_attempts' do
+  erb :delivery_attempts
+end
